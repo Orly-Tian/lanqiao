d0f73e52ba2bceed94919328a11aafcbbda6977a
Orly <76263061+Orly-Tian@users.noreply.github.com>
2024-10-12 23:24:53 +0800

    Create date_10_12.c
    “To iterate is human, to recurse divine.”（迭代的是人，递归的是神）

---
+++ date_10_12.c
@@ -0,0 +1,14 @@
+//
+// Created by Administrator on 2024/10/12.
+// 致敬数学爱好者——拉马努金
+
+#include <stdio.h>
+
+int main() {
+    
+    printf("To iterate is human, to recurse divine.");
+    
+
+    return 0;
+}
+
