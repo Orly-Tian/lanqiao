4f213a2af4a284f9e5e07469142cb117645ea876
Orly <76263061+Orly-Tian@users.noreply.github.com>
2024-10-27 23:51:18 +0800

    Update date_10_24.c
    现在，给定一个自然数 S，请你找到一个自然数 T，使得将 S、T 转换为数字字符串后，（数字字符串）T 的字典序大于（数字字符串）S，并且在所有满足此条件的数字字符串中，（数字字符串）T 的字典序是最小的。

--- date_10_24.c
+++ date_10_24.c
@@ -5,30 +5,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// int main(int argc, char *argv[])
+// {
+//     // 请在此输入您的代码
+//     int n;
+//     scanf("%d", &n);
+//     for (int i = 0; i <= n; i++) {
+//         char S[5] = {0};
+//         int length = 0;
+//         char ch;
+//         while ((ch = getchar()) != '\n') {
+//             S[length] = ch;
+//             length++;
+//         }
+//         for (int j = 0; j < length; j++) {
+//             if (j == length-1) {
+//                 if (S[j] == '9') {
+//                     printf("%s0\n", S);
+//                 }
+//                 else {
+//                     S[j]++;
+//                     printf("%s\n", S);
+//                 }
+//             }
+//         }
+//     }
+//
+//     return 0;
+// }
+//
+
+
+/*  第一次做：
+ *  1.初始化
+ *  2.若此数尾数为9，则加一位0
+ *    若此数尾数非9，则此数+1
+ *  3.输出
+ *
+ *
+ *  竞赛中心：
+ *  1.初始化
+ *  2.若此数非0，则加一位0
+ *    若此数为0，则此数+1(因为C中整型不能有前导0)
+ *  3.输出
+ */
+
+
 int main(int argc, char *argv[])
 {
-    // 请在此输入您的代码
     int n;
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
-        char S[9999];
-        int length = 0;
-        while ((S[length] = getchar()) != '\n') {
-            length++;
+    scanf("%d",&n);
+    long long int a[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%lld",&a[i]);
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]==0)
+        {
+            printf("1\n");
         }
-        for (int j = 0; j <= length; j++) {
-            if (j == length-1) {
-                if (S[j] == '9') {
-                    printf("%s0", S);
-                }
-                else {
-                    S[j]++;
-                    printf("%s\n", S);
-                }
-            }
-        }
+        else printf("%lld\n",10*a[i]);
     }
 
     return 0;
 }
-
