d9ea1853953694254b8490e4b8c2ba0f395ae4b9
Orly <76263061+Orly-Tian@users.noreply.github.com>
2024-10-29 22:01:34 +0800

    Create date_10_29.c
    给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
    
    请你将两个数相加，并以相同形式返回一个表示和的链表。
    
    你可以假设除了数字 0 之外，这两个数都不会以 0 开头。

---
+++ date_10_29.c
@@ -0,0 +1,92 @@
+//
+// Created by Administrator on 2024/10/28.
+// https://leetcode.cn/problems/add-two-numbers/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+
+    // 创建结果头指针尾指针
+    struct ListNode *head = NULL, *tail = NULL;
+
+    // 初始化进位为0
+    int carry = 0;
+
+    // 遍历链表l1与l2
+    while (l1 || l2) {
+        // 若较短的链表遍历完则将链表数据域视为0
+        int n1 = l1 ? l1->val : 0;
+        int n2 = l2 ? l2->val : 0;
+        // 提取出来的值求和并加上进位
+        int sum = n1 + n2 + carry;
+
+        // 若结果链表此时为NULL
+        if (!head) {
+            // 为结果链表新建一个节点
+            head = tail = malloc(sizeof(struct ListNode));
+            // 新节点值为求和值的个位数
+            tail->val = sum % 10;
+            // 尾节点的指针域为NULL
+            tail->next = NULL;
+        }
+        // 若结果链表此时有值
+        else {
+            // 尾插法插入新节点
+            tail->next = malloc(sizeof(struct ListNode));
+            // 新节点值为求和值的个位数
+            tail->next->val = sum % 10;
+            tail = tail->next;
+            tail->next = NULL;
+        }
+        // 更新进位
+        carry = sum / 10;
+
+        // 循环计数器
+        if (l1)
+            l1 = l1->next;
+        if (l2)
+            l2 = l2->next;
+    }
+
+    // 若两链表皆遍历结束后仍有进位
+    if (carry > 0) {
+        // 则先将进位值赋值给新节点
+        tail->next = malloc(sizeof(struct ListNode));
+        tail->next->val = carry;
+        tail->next->next = NULL;
+    }
+
+    return head;
+}
+
+
+/*  第一次做：
+ *  1.新建结果链表的头节点,并分配节点
+ *  2.不会
+ *
+ *
+ *  leetcode官方题解:
+ *  1.新建结果链表的头尾节点
+ *  2.初始化进位
+ *    遍历两数组,
+ *      若链表有值,将链表值提取出来
+ *      若链表无值,则视为0
+ *
+ *      若结果链表无节点,
+ *          则分配新节点,值为提取出来的两值加进位
+ *      若结果链表有节点,
+ *          则尾插法分配新节点,值为提取出来的两值加进位
+ *      更新进位
+ *      循环计数器
+ *  3.若循环结束仍有进位,
+ *      则新建节点保存进位制
+ *
+ */
+
