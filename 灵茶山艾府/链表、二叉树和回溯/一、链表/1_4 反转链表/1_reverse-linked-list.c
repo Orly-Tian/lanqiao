8094faa509b4e8c90beefe644c0e5876032b7731
Orly <76263061+Orly-Tian@users.noreply.github.com>
2024-12-20 23:33:41 +0800

    Update 1_reverse-linked-list.c

--- 灵茶山艾府/链表、二叉树和回溯/一、链表/1_4 反转链表/1_reverse-linked-list.c
+++ 灵茶山艾府/链表、二叉树和回溯/一、链表/1_4 反转链表/1_reverse-linked-list.c
@@ -10,27 +10,57 @@
  * };
  */
 
-
 #include <stddef.h>
 
 
-// 双指针
+// 递归
+//		(思路同双指针)
 struct ListNode* reverseList(struct ListNode* head) {
-	// current指针遍历链表,pre指向current的前一个节点,也就是反转后current的下一个节点
-    struct ListNode *current = head, *pre = NULL;
+	// 声明函数
+	struct ListNode* reverse(struct ListNode *head, struct ListNode *pre);
 
-	while (current) {
-		// after指向current的下一个节点
-		struct ListNode *after = current->next;
+	// 创建遍历链表的current指针与当前的pre指针
+	struct ListNode *current = head, *pre = NULL;
 
-		// 反转当前节点
-		current->next = pre;
-
-		// 移动指针
-		pre = current;
-		current = after;
-	}
-
-	// 返回反转后链表的头指针,此时current为NULL,头指针为pre
-	return pre;
+	// 递归函数返回结果
+	return reverse(current, pre);
 }
+
+
+struct ListNode* reverse(struct ListNode *current, struct ListNode *pre) {
+	// 设置基例,当递归到最深处函数时返回最后结果
+	// 此时current为NULL,pre为原链表的最后一个节点,即反转链表后的头节点
+	if (current == NULL)
+		return pre;
+
+	// 设置链条
+	// 反转当前节点的方向
+	struct ListNode *after = current->next;
+	current->next = pre;
+
+	// 递归函数,并把两参数都后移一位
+	return reverse(after, current);
+}
+
+
+// 双指针
+// struct ListNode* reverseList(struct ListNode* head) {
+// 	// current指针遍历链表,pre指向current的前一个节点,也就是反转后current的下一个节点
+//     struct ListNode *current = head, *pre = NULL;
+//
+// 	while (current) {
+// 		// after指向current的下一个节点
+// 		struct ListNode *after = current->next;
+//
+// 		// 反转当前节点
+// 		current->next = pre;
+//
+// 		// 移动指针
+// 		pre = current;
+// 		current = after;
+// 	}
+//
+// 	// 返回反转后链表的头指针,此时current为NULL,头指针为pre
+// 	return pre;
+// }
+//
