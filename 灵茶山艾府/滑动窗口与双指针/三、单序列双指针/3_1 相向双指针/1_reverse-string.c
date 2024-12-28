aa38f7249ee77ac9254ba51569dd33dbaca3fd61
Orly <76263061+Orly-Tian@users.noreply.github.com>
2024-12-28 23:47:08 +0800

    Update 1_reverse-string.c

--- 灵茶山艾府/滑动窗口与双指针/三、单序列双指针/3_1 相向双指针/1_reverse-string.c
+++ 灵茶山艾府/滑动窗口与双指针/三、单序列双指针/3_1 相向双指针/1_reverse-string.c
@@ -32,13 +32,41 @@ int main() {
 
 
 char* reverseStr(char* s, int k) {
-    int i = 0;
-    while (i < k) {
-        if (i == k - 1)
+    // i为保持在2k内部的遍历指针
+    // index为整个字符串的遍历指针
+    // count为2k的倍数
+    int i = 0, index = 0, count = 0;
 
+    // i在2k区间内&&index在字符串区间内
+    while (i < 2 * k && index < strlen(s)) {
+        // 2k区间左右向内收缩且交换位置
+        int left = index, right = (count * 2 * k)+ k - i - 1;
+        if (i < k && left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+        }
 
-        i++;
+        // 更新计数器
+        i++, index++;
+
+        // 若i值超过2k的范围,则重置
         if (i == 2 * k)
-            i = 0;
+            i = 0, count++;
     }
+
+    return s;
 }
+
+
+/*  第一次做:
+ *      (通过9/60个测试用例)
+ *
+ *  问题:
+ *      循环变量i只在2k内遍历循环,则只能在第一个2k区间内给left和right正确赋值
+ *          通过添加index变量,用于遍历整个字符串解决
+ *      区间的right变量赋值不正确(right = index + k - i - 1)
+ *          right的值应为区间内的左端-i-1,而index为已经收缩后的left变量
+ *          所以又加入count变量,用于记录此时循环至第几个2k区间,count值即为此刻2k区间的左端点
+ *
+ */
