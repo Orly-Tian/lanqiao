8138b3e76efdc8702e072ccee2359b0dd3ccf1f5
Orly <76263061+Orly-Tian@users.noreply.github.com>
2025-01-16 22:40:52 +0800

    Update 1_binary-tree-preorder-traversal.c

--- 灵茶山艾府/常用数据结构/_六、二叉树/1_binary-tree-preorder-traversal.c
+++ 灵茶山艾府/常用数据结构/_六、二叉树/1_binary-tree-preorder-traversal.c
@@ -30,3 +30,42 @@ int* preorderTraversal(struct TreeNode* root, int* returnSize) {
     preOrder(root, ret, returnSize);
     return ret;
 }
+
+
+
+// 94.二叉树的中序遍历
+// https://leetcode.cn/problems/binary-tree-inorder-traversal/
+void inOrder(struct TreeNode *root, int *result, int *resultSize) {
+    if (root == NULL)
+        return;
+    inOrder(root->left, result, resultSize);
+    result[(*resultSize)++] = root->val;
+    inOrder(root->right, result, resultSize);
+}
+
+
+int* inorderTraversal(struct TreeNode* root, int* returnSize) {
+    int *result = (int *)malloc(100 * sizeof(int));
+    *returnSize = 0;
+    inOrder(root, result, returnSize);
+    return result;
+}
+
+
+
+// 145.二叉树的后序遍历
+void postOrder(struct TreeNode *root, int *result, int *resultSize) {
+    if (root == NULL)
+        return;
+    postOrder(root->left, result, resultSize);
+    postOrder(root->right, result, resultSize);
+    result[(*resultSize)++] = root->val;
+}
+
+
+int* postorderTraversal(struct TreeNode* root, int* returnSize) {
+    int *result = (int *)malloc(100 * sizeof(int));
+    *returnSize = 0;
+    postOrder(root, result, returnSize);
+    return result;
+}
