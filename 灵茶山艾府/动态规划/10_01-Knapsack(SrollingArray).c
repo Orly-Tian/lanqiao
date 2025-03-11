aa1a420ec378af3b3f79b7c547df6a5163abfc82
Orly <76263061+Orly-Tian@users.noreply.github.com>
2025-03-11 22:56:39 +0800

    Create 10_01-Knapsack(SrollingArray).c

---
+++ 灵茶山艾府/动态规划/10_01-Knapsack(SrollingArray).c
@@ -0,0 +1,41 @@
+// 动态规划DP：01背包(滚动数组优化)
+// Created by Administrator on 2025/3/10.
+// https://www.bilibili.com/video/BV1BU4y177kY
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+	// 物品数量与背包容量
+	int n, bagweight;
+	scanf("%d %d", &n, &bagweight);
+
+	// 物品的重量与价值
+	int *weight = (int *)malloc(n * sizeof(int));
+	for (int i = 0; i < n; i++)
+		scanf("%d", &weight[i]);
+	int *value = (int *)malloc(n * sizeof(int));
+	for (int i = 0; i < n; i++)
+		scanf("%d", &value[i]);
+
+	// 定义dp数组，dp[j]表示为背包在容量为j的情况下所能装的最大价值
+	int dp[bagweight + 1];
+
+	dp[0] = 0;
+
+	// 遍历物品
+	for (int i = 0; i < n; i++) {
+		dp[i] = 0;
+		// 倒序遍历背包容量，为了防止同一物品被多次添加
+		for (int j = bagweight; j >= weight[i]; j--)
+			// 取 未放物品dp[j] 与 放此物品dp[j - weight[i] + value[i]] 的最大值
+			dp[j] = fmax(dp[j], dp[j - weight[i]] + value[i]);
+	}
+
+	printf("%d", dp[bagweight]);
+
+	return 0;
+}
+
