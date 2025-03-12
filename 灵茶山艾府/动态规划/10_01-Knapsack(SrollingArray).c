8bfb328c3968e1b6c33583ab959a5471a7ef59c2
Orly <76263061+Orly-Tian@users.noreply.github.com>
2025-03-12 22:55:15 +0800

    Update 10_01-Knapsack(SrollingArray).c

--- 灵茶山艾府/动态规划/10_01-Knapsack(SrollingArray).c
+++ 灵茶山艾府/动态规划/10_01-Knapsack(SrollingArray).c
@@ -21,20 +21,26 @@ int main() {
 		scanf("%d", &value[i]);
 
 	// 定义dp数组，dp[j]表示为背包在容量为j的情况下所能装的最大价值
-	int dp[bagweight + 1];
-
-	dp[0] = 0;
+	int *dp = (int *)malloc((bagweight + 1) * sizeof(int));
+	// 初始化dp
+	for (int j = 0; j <= bagweight; j++)
+		dp[j] = 0;
 
 	// 遍历物品
 	for (int i = 0; i < n; i++) {
-		dp[i] = 0;
 		// 倒序遍历背包容量，为了防止同一物品被多次添加
 		for (int j = bagweight; j >= weight[i]; j--)
 			// 取 未放物品dp[j] 与 放此物品dp[j - weight[i] + value[i]] 的最大值
 			dp[j] = fmax(dp[j], dp[j - weight[i]] + value[i]);
 	}
 
+	// 输出最大价值
 	printf("%d", dp[bagweight]);
+
+	// 释放内存，防止内存泄露
+	free(weight);
+	free(value);
+	free(dp);
 
 	return 0;
 }
