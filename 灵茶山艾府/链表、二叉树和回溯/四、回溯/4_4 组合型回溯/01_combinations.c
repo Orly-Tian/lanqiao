553cc8f813bc9487db35a463ed754af2c746393a
Orly <76263061+Orly-Tian@users.noreply.github.com>
2025-03-20 23:06:24 +0800

    Update 01_combinations.c

--- 灵茶山艾府/链表、二叉树和回溯/四、回溯/4_4 组合型回溯/01_combinations.c
+++ 灵茶山艾府/链表、二叉树和回溯/四、回溯/4_4 组合型回溯/01_combinations.c
@@ -13,9 +13,10 @@
 
 
 // 一维数组栈，存放组合数结果
-int *path, pathTop;
+int *path;
 // 二维数组栈，存放所有的组合数结果
-int **result, resultTop;
+int **result;
+int pathTop, resultTop;
 
 
 // n为题目范围
@@ -31,8 +32,9 @@ void backtrackting(int n, int k, int start) {
 		result[resultTop++] = temp;
 		return;
 	}
+
 	// 单层递归逻辑：从start遍历到原数组末尾
-	for (int j = start; j <= n - (k - pathTop) + 1; j++) {
+	for (int j = start; j <= n; j++) {
 		// 添加元素至栈
 		path[pathTop++] = j;
 		// 递归函数，start值后移一位
@@ -47,18 +49,25 @@ int** combine(int n, int k, int* returnSize, int** returnColumnSizes) {
 	// path为树的路径，即用数组存放组合结果
     path = (int *)malloc(k * sizeof(int));
 	// result为所有组合的二维数组
-	result = (int **)malloc(10000 * sizeof(int *));
+	result = (int **)malloc(200000 * sizeof(int *));
 	pathTop = resultTop = 0;
 
+	// 组合数范围从1到n
 	backtrackting(n, k, 1);
 
 	// 二维数组长度为resultTop的值
 	*returnSize = resultTop;
-	*returnColumnSizes = (int *)malloc((*returnSize) * sizeof(int));
 	// 将二维数组中的所有一维数组长度赋值为k
+	*returnColumnSizes = (int *)malloc((*returnSize) * sizeof(int));
 	for (int i = 0; i < *returnSize; i++)
 		(*returnColumnSizes)[i] = k;
 
 	return result;
 }
 
+
+// 第32行一直堆溢出:
+//		==22==ERROR: AddressSanitizer: heap-buffer-overflow
+// 未通过测试用例为:
+//		n = 20, k = 10
+// 原因仅是二维数组开小了，当为上述测试用例时二维数组有184756个结果
