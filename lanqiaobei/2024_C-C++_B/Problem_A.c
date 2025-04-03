e0512b7becf60b484d53c472cf266bb018daa390
Orly <76263061+Orly-Tian@users.noreply.github.com>
2025-04-03 23:31:49 +0800

    Update Problem_A.c

--- lanqiaobei/2024_C-C++_B/Problem_A.c
+++ lanqiaobei/2024_C-C++_B/Problem_A.c
@@ -4,19 +4,29 @@
 
 #include <stdio.h>
 
-int main() {
-	int count = 0;
-	for (int i = 49; i > 0; i--) {
-		count += i;
-	}
+// int main() {
+// 	int count = 0;
+// 	for (int i = 49; i > 0; i--) {
+// 		count += i;
+// 	}
+//
+// 	int count_not = 0;
+// 	for (int i = 6; i > 0; i--) {
+// 		count_not += i;
+// 	}
+//
+// 	printf("%d", count - count_not);
+//
+// 	return 0;
+// }
 
-	int count_not = 0;
-	for (int i = 6; i > 0; i--) {
-		count_not += i;
-	}
 
-	printf("%d", count - count_not);
 
-  return 0;
+int main(int argc, char *argv[])
+{
+	// 请在此输入您的代码
+	printf("%d", ((49 * 43) + (43 * 7)) / 2);
+
+
+	return 0;
 }
-
